aa0b932a4a4efc428d7b028998bae7da16100c7a
Daiming Yang <yang.daim@northeastern.edu>
2024-01-11 11:30:12 +0800

    feat(typescript): :sparkles: generics in ts

---
+++ typescript/generics.ts
@@ -0,0 +1,10 @@
+function echo<T> (input: T) : T {
+    return input
+}
+
+const output: string = echo('123'); //the input and the output have consistent types
+
+function swap<T, U> (tuple: [T, U]): [U, T] {
+    return [tuple[1], tuple[0]]
+}
+const swapped: [string, number] = swap([123, '123'])
