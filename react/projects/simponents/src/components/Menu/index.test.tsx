1542fe596d352abd9407f10e81e5e840db5c9f1f
Daiming Yang <yang.daim@northeastern.edu>
2024-01-14 17:36:20 +0800

    feat(react): :sparkles: add menu

--- react/projects/simponents/src/components/Menu/index.test.tsx
+++ react/projects/simponents/src/components/Menu/index.test.tsx
@@ -100,5 +100,11 @@ describe("test Menu and MenuItem component", () => {
     await waitFor(() => {
       expect(wrapper.queryByText("drop1")).toBeVisible();
     });
+    fireEvent.click(wrapper.getByText("drop1"));
+    expect(testProps.onSelect).toHaveBeenCalledWith("3-0");
+    fireEvent.mouseLeave(dropdownElement);
+    await waitFor(() => {
+      expect(wrapper.queryByText("drop1")).not.toBeVisible();
+    });
   });
 });
