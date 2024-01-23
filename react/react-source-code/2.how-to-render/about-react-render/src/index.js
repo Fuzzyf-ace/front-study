b2bb9e6e28787aa8cbbf8e594b9a7b37e76882b7
Daiming Yang <yang.daim@northeastern.edu>
2024-01-23 16:38:41 +0800

    feat(react): :sparkles: virtual DOM concept explain

--- react/react-source-code/2.how-to-render/about-react-render/src/index.js
+++ react/react-source-code/2.how-to-render/about-react-render/src/index.js
@@ -1,4 +1,37 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>React</div>);
+// get real dom by browser api
+const element = document.getElementById("root");
+const obj = {};
+for (let key in element) {
+  obj[key] = element[key];
+}
+const obj2 = {};
+// create virtual dom by jsx
+const ele = <div>this is virtual</div>;
+for (let key in ele) {
+  obj2[key] = ele[key];
+}
+root.render(
+  <div>
+    <div>
+      Real DOM
+      {Object.keys(obj).map((key) => (
+        <div key={key}>
+          <span style={{ color: "red" }}>{key}</span> :{" "}
+          {obj[key] && obj[key].toString()}
+        </div>
+      ))}
+    </div>
+    <div>
+      Virtual DOM
+      {Object.keys(obj2).map((key) => (
+        <div key={key}>
+          <span style={{ color: "green" }}>{key}</span> :{" "}
+          {obj2[key] && obj2[key].toString()}
+        </div>
+      ))}
+    </div>
+  </div>
+);
