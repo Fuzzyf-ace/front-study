bc7f0a178bd431741c9d72a659110fcf55660221
Daiming Yang <yang.daim@northeastern.edu>
2024-01-25 21:25:33 +0800

    refactor(react): refract indexes

---
+++ react/react-source-code/2.how-to-render/about-react-render/src/indexes/2.createElement&Render.js
@@ -0,0 +1,36 @@
+import ReactDOM from "react-dom";
+// import React from "react";
+import React, { REACT_ELEMENT } from "./react";
+// import ReactDOM from "./react-dom";
+// create a virtual element using Babel to compile JSX to React.createElement(js)
+const ele = (
+  <div
+    className="classname"
+    style={{ color: "red" }}
+    onClick={() => {
+      console.log("dafs");
+    }}
+  >
+    this is virtual
+    <span>
+      <span> inner span </span>
+      span
+    </span>
+  </div>
+);
+
+// mount created elements to real dom
+ReactDOM.render(
+  //   React.createElement("div", {}, "Hello JSX"),
+  //   {
+  //     // this is a virtual dom demo, the return value of React.createElement function
+  //     $$typeof: REACT_ELEMENT,
+  //     type: "div",
+  //     props: { children: "Hello JSX" },
+  //     ref: null,
+  //     key: null,
+  //   },
+  ele,
+  document.getElementById("root")
+);
+console.log("ele", ele);
