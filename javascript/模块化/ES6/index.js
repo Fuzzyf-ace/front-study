d03d9e1af721efed2b2d164a857c0e9b4b3286a7
Daiming Yang <yang.daim@northeastern.edu>
2024-09-02 13:27:26 +0800

    last time to dump all

--- javascript/模块化/ES6/index.js
+++ javascript/模块化/ES6/index.js
@@ -3,7 +3,7 @@ import * as school from "./school.js";
 import { name, address } from "./school.js"; // named import
 import { nameE as nameB, addresE as addressB } from "./school.js"; // alias import
 import defaultNumber from "./school.js"; // default import
-
+console.log("index.js 被执行");
 console.log(
   `{ name: ${name}, address: ${address}; nameB: ${nameB}, addressB: ${addressB}; defaultNumber: ${defaultNumber} }`
 );
--- javascript/模块化/ES6/school.js
+++ javascript/模块化/ES6/school.js
@@ -2,4 +2,5 @@ export const name = "school";
 export const address = "address";
 
 export { name as nameE, address as addresE };
+console.log("school.js被执行");
 export default 900;
